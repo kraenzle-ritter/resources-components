e51df41b6f71663829da7b2348f2e9f7f76946db
andreas_kraenzle <kraenzle@k-r.ch>
2024-04-06 19:05:02 +0200

    Update ProviderSelect.php

--- src/ProviderSelect.php
+++ src/ProviderSelect.php
@@ -20,7 +20,7 @@ class ProviderSelect extends Component
 
     public function mount($model, $providers, $endpoint)
     {
-        logger(__METHOD__, [$this->provider]);
+        //logger(__METHOD__, [$this->provider]);
         $this->model = $model;
         $this->endpoint = $endpoint;
         $this->providers_all = array_map('strtolower', $providers);
@@ -31,7 +31,7 @@ class ProviderSelect extends Component
 
     public function hydrate()
     {
-        logger(__METHOD__, [$this->provider]);
+        //logger(__METHOD__, [$this->provider]);
         $this->model->load('resources');
         $linked_providers = $this->model->resources->pluck('provider')->toArray();
         $this->providers = array_values(array_diff($this->providers_all, $linked_providers));
@@ -40,14 +40,14 @@ class ProviderSelect extends Component
 
     public function render()
     {
-        logger(__METHOD__, [$this->provider]);
+        //logger(__METHOD__, [$this->provider]);
         $this->model->load('resources');
         $linked_providers = $this->model->resources->pluck('provider')->toArray();
 
         $this->providers = array_values(array_diff($this->providers_all, $linked_providers));
         $this->provider = $this->providers[0] ?? '';
 
-        logger(__METHOD__, [$this->provider]);
+        //logger(__METHOD__, [$this->provider]);
         $view = view()->exists('vendor.kraenzle-ritter.livewire.provider-select')
               ? 'vendor.kraenzle-ritter.livewire.provider-select'
               : 'resources-components::provider-select';
